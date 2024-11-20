f9b733d134a1a76a6ba138a4b6300bdfcd6553a9
AiLoverer <447533920@qq.com>
2024-11-20 14:10:10 +0800

    add Toggle button demo

---
+++ Activity/Application/configs/platforms/android_gradle/app/src/main/java/com/example/activity/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.rightware.kanzi.Domain;
+import com.rightware.kanzi.KanziView;
+import com.rightware.kanzi.KanziViewListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        // Inflate activity contents from its layout.
+        setContentView(R.layout.activity_main);
+
+        // Get a reference to your View.
+        KanziView view = findViewById(R.id.activityview);
+
+        // Add state change callbacks to the View.
+        view.addListener(new KanziViewListener() {
+            @Override
+            public void onAttachedToWindow(View view, Domain domain) {
+                KanziGenerated.registerPlugins(domain);
+            }
+
+            @Override
+            public void onStartupPrefabLoaded(View view, Domain domain) {
+            }
+        });
+    }
+}
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: XQ
  * @Date: 2024-11-19 10:37:22
- * @LastEditTime: 2024-11-19 10:44:35
+ * @LastEditTime: 2024-11-20 09:26:30
  * @LastEditors: XQ
  * @Description: 
  * @FilePath: \undefinedd:\xuqiang\github\Kanzi-Demos\readme.md
@@ -11,8 +11,32 @@
 
 # 仓库介绍和说明
 ## Getting started
-主要熟悉Kanzi里面的一些简单流程：按钮 状态切换 属性控制 资源加载 等。
+在本仓库的这一步中主要学习：  
+- 熟悉Kanzi里面的一些简单流程
+- 按钮 
+- 状态切换 
+- 属性控制 
+- 资源加载 
 ![效果图](./res/getting-started-completed.gif)  
 ## First Application
-主要属性Kanzi状态切换 Trajectory List Box使用 效卡片制作 布局等。  
-![效果图](./res/First%20Application.gif)
+在本仓库的这一步中主要学习：  
+- 属性
+- Kanzi状态切换 
+- Trajectory List Box使用 
+- 效卡片制作 
+- 布局  
+![效果图](./res/First%20Application.gif)  
+## Activity
+在本仓库的这一步中主要学习：
+
+- 关于不同类型的Activity节点
+
+- 如何使用活动浏览器创建具有活动节点的应用程序结构
+
+- 如何使用状态管理器控制活动节点的激活状态
+
+## Components
+在本仓库的这一步中主要学习自定义控件：
+- Toggle Button  
+
+![效果图](./res/toggle-button-final.gif)
